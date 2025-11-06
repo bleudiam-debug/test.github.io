1462ae172675e4949d4298ab02320419338600ba
bleudiam-debug <bleudiam@gmail.com>
2025-11-06 11:03:03 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -53,7 +53,7 @@ burgerIcon.addEventListener("click", () => {
 
 async function classify() {
   const vehicle = document.getElementById("vehicle").value;
-  const res = await fetch("http://localhost:3000/api/classify", {
+  const res = await fetch("https://test-github-io-fc30.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ vehicle }),
